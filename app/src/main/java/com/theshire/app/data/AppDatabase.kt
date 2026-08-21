051ca080fe976309f2a655624ed7a728b7df9649
Uschglu <hoffner.robin@gmail.com>
2026-08-21 19:01:45 +0200

    Update AppDatabase.kt

--- app/src/main/java/com/theshire/app/data/AppDatabase.kt
+++ app/src/main/java/com/theshire/app/data/AppDatabase.kt
@@ -24,7 +24,9 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "potager_db"
-                ).build()
+                )
+                .fallbackToDestructiveMigration()
+                .build()
                 INSTANCE = instance
                 instance
             }
