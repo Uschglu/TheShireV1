765c3447e4ca3f0f336315798434263d4f79ae8f
Uschglu <hoffner.robin@gmail.com>
2026-08-21 15:35:06 +0200

    Create AppDatabase.kt

---
+++ app/src/main/java/com/theshire/app/data/AppDatabase.kt
@@ -0,0 +1,33 @@
+package com.theshire.app.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [LegumeEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    
+    abstract fun legumeDao(): LegumeDao
+    
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+        
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "potager_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
