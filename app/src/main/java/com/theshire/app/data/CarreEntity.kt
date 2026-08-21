d265cb714586a7d181eb7d16bd812a86a3c356f3
Uschglu <hoffner.robin@gmail.com>
2026-08-21 20:37:21 +0200

    Create CarreEntity.kt

---
+++ app/src/main/java/com/theshire/app/data/CarreEntity.kt
@@ -0,0 +1,21 @@
+package com.theshire.app.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "carres")
+data class CarreEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val plancheId: Long,
+    val position: Int,
+    val case1: String? = null,
+    val case2: String? = null,
+    val case3: String? = null,
+    val case4: String? = null,
+    val case5: String? = null,
+    val case6: String? = null,
+    val case7: String? = null,
+    val case8: String? = null,
+    val case9: String? = null
+)
