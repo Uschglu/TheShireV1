4a753c2e8dbc2874b651f6616feb80a33e182838
Uschglu <hoffner.robin@gmail.com>
2026-08-21 15:34:31 +0200

    Create LegumeDao interface for database operations

---
+++ app/src/main/java/com/theshire/app/data/LegumeDao.kt
@@ -0,0 +1,20 @@
+package com.theshire.app.data
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface LegumeDao {
+    
+    @Query("SELECT * FROM legumes ORDER BY nom")
+    fun getAllLegumes(): Flow<List<LegumeEntity>>
+    
+    @Insert
+    suspend fun insertLegume(legume: LegumeEntity)
+    
+    @Delete
+    suspend fun deleteLegume(legume: LegumeEntity)
+    
+    @Query("DELETE FROM legumes")
+    suspend fun deleteAllLegumes()
+}
