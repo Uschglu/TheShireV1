2bbce9d90d92d8d312866e5f36c98b839b50e791
Uschglu <hoffner.robin@gmail.com>
2026-08-22 09:17:36 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,11 @@ dependencies {
     // Pour la géolocalisation
     implementation("com.google.android.gms:play-services-location:21.3.0")
     
+    // Pour la caméra
+    implementation("androidx.camera:camera-camera2:1.3.4")
+    implementation("androidx.camera:camera-lifecycle:1.3.4")
+    implementation("androidx.camera:camera-view:1.3.4")
+    
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
