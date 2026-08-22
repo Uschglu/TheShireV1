e82cef6158da03d6f90b39e697bb95f153c02480
Uschglu <hoffner.robin@gmail.com>
2026-08-22 07:32:44 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,10 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.1")
     implementation("androidx.activity:activity-compose:1.9.0")
     
+    // Pour les appels réseau (météo)
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.json:json:20240303")
+    
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
