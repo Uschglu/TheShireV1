510d5342b3a5ba84c56d5403cc4b8b66c013a3ee
Uschglu <hoffner.robin@gmail.com>
2026-09-19 21:57:15 +0200

    Add import for InfoCard component

--- app/src/main/java/com/theshire/app/ui/EcranAgricultureUrbaine.kt
+++ app/src/main/java/com/theshire/app/ui/EcranAgricultureUrbaine.kt
@@ -22,6 +22,7 @@ import com.theshire.app.data.AgricultureUrbaine
 import com.theshire.app.data.ConseilUrbain
 import com.theshire.app.data.MaterielUrbain
 import com.theshire.app.data.PlanteUrbaine
+import com.theshire.app.ui.components.InfoCard
 import com.theshire.app.ui.theme.CouleursApp
 
 /**
