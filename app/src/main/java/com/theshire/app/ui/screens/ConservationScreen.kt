e0bac819b5647a16c23989437c3507bd5994f4d1
Uschglu <hoffner.robin@gmail.com>
2026-09-19 21:55:27 +0200

    Reorder import statements in ConservationScreen.kt

--- app/src/main/java/com/theshire/app/ui/screens/ConservationScreen.kt
+++ app/src/main/java/com/theshire/app/ui/screens/ConservationScreen.kt
@@ -1,7 +1,6 @@
 package com.theshire.app.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
